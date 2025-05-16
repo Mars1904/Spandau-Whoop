dfed849704ad749fde7e77e16189aa34c033c6c9
Mars1904 <47983761+Mars1904@users.noreply.github.com>
2025-05-16 14:01:55 +0200

    webhook.ts

--- api/webhook.ts
+++ api/webhook.ts
@@ -1,1 +1,6 @@
-// Webhook handler
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default (req: VercelRequest, res: VercelResponse) => {
+  console.log("Webhook Data Received:", req.body);
+  res.status(200).send('Webhook received successfully.');
+};
