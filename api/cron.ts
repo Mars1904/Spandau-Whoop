d40589eef1bbc29a4b4ab0880ecaf580a7db44a7
Mars1904 <47983761+Mars1904@users.noreply.github.com>
2025-05-16 14:43:23 +0200

    Delete api/cron.ts

--- api/cron.ts
+++
@@ -1,6 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default (req: VercelRequest, res: VercelResponse) => {
-  console.log("Cron executed at:", new Date());
-  res.status(200).send('Cron job executed successfully.');
-};
