6a02bef52aaaefb511218253322376664dcbc14b
Mars1904 <47983761+Mars1904@users.noreply.github.com>
2025-05-16 14:02:20 +0200

    cron.ts

--- api/cron.ts
+++ api/cron.ts
@@ -1,1 +1,6 @@
-// Cronjob handler
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default (req: VercelRequest, res: VercelResponse) => {
+  console.log("Cron executed at:", new Date());
+  res.status(200).send('Cron job executed successfully.');
+};
