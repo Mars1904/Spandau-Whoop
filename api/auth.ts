492d51dc5fbcd292d86435d9de9d65fd38dcd87b
Mars1904 <47983761+Mars1904@users.noreply.github.com>
2025-05-16 14:01:30 +0200

    auth.ts

--- api/auth.ts
+++ api/auth.ts
@@ -1,35 +1,26 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import fetch from 'node-fetch';
 
-const clientId = process.env.WHOOP_CLIENT_ID;
-const clientSecret = process.env.WHOOP_CLIENT_SECRET;
-const redirectUri = process.env.REDIRECT_URI;
-
 export default async (req: VercelRequest, res: VercelResponse) => {
-  const code = req.query.code as string;
+  const { code } = req.query;
+  const { WHOOP_CLIENT_ID, WHOOP_CLIENT_SECRET, REDIRECT_URI } = process.env;
 
   if (!code) {
-    return res.status(400).send('Code is missing');
+    return res.status(400).send('Code missing');
   }
 
   const response = await fetch('https://api.prod.whoop.com/oauth/oauth2/token', {
     method: 'POST',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
     body: new URLSearchParams({
+      client_id: WHOOP_CLIENT_ID!,
+      client_secret: WHOOP_CLIENT_SECRET!,
+      redirect_uri: REDIRECT_URI!,
       grant_type: 'authorization_code',
-      code,
-      redirect_uri: redirectUri,
-      client_id: clientId,
-      client_secret: clientSecret,
+      code: code as string,
     }),
   });
 
-  if (!response.ok) {
-    return res.status(response.status).send('Token request failed');
-  }
-
   const data = await response.json();
-
-  // Testausgabe:
-  res.status(200).json(data);
+  res.status(response.status).json(data);
 };
